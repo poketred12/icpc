2f2809050068c570d2ce07e694b702e38ad03aff
silver <zmtn94@users.noreply.github.com>
2018-08-18 16:36:09 +0900

    Add files via upload

---
+++ 6thWeek/JOSEPHUS/soonhyun.cpp
@@ -0,0 +1,44 @@
+﻿#include <string>
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <map>
+#include <functional>
+#include <math.h>
+#include <deque>
+
+using namespace std;
+typedef long long ll; 
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);  
+	int tc; cin >> tc;
+	while (tc--) {
+		int n, k;
+		cin >> n >> k; 
+		deque<int> dq;
+		for (int i = 1; i <= n; i++) {
+			dq.push_back(i);
+		}
+
+		while (dq.size() != 2) {
+			dq.pop_front(); 
+
+			for (int i = 0; i < k - 1; i++) {
+				int tmp = dq.front();
+				dq.pop_front();
+				dq.push_back(tmp);
+			}
+		}
+
+		sort(dq.begin(), dq.end());
+		for (auto print : dq) {
+			cout << print << " ";
+		}
+		cout << "\n";
+	}
+}
