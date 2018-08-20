a657933b9eba847845a870c7bdb006402bc07822
silver <zmtn94@users.noreply.github.com>
2018-08-20 13:39:29 +0900

    Update soonhyun.cpp

--- 7thWeek/ITES/soonhyun.cpp
+++ 7thWeek/ITES/soonhyun.cpp
@@ -1,15 +1,7 @@
-#include <string>
-#include <iostream>
-#include <stdio.h>
+#include <iostream> 
 #include <vector>
 #include <algorithm>
-#include <queue>
-#include <set>
-#include <map>
-#include <functional>
-#include <math.h>
-#include <deque>
-#include <stack>
+#include <queue> 
 
 using namespace std;
 typedef long long ll; 
@@ -24,15 +16,24 @@ int main() {
 	int tc; cin >> tc;
 	while (tc--) {
 		int k, n;  cin >> k >> n;
-		preva = 1983; 
+		preva = 1983; // A[i-1]
 		ll MOD = 1LL << 32;
 		ll ans = 0;
 		ll total = 0;
+
 		for (int i = 1; i <= n; i++) {
-			cura = (preva * 214013 + 2531011) % MOD;
-			signal = preva % 10000 + 1;
+			cura = (preva * 214013 + 2531011) % MOD; // A[i]
+			signal = preva % 10000 + 1; //현재 신호를 담고있는 변수 
 			preva = cura; 
-			if (signal > k) {
+
+			if (signal > k) { 
+				/*
+				*
+				*   만약 signal이 k보다 크다면 큐안에 있는 값들 사용하지 못함
+				*   신호가 1 4 8 3 2 8 1 1 1 3 8 4 3 8이고, K가 7일 때
+				*   1 4 8(끊기고) 3 2 8(끊기고) 1 1 1 3 8(끊기고) 4 3(ans++) 8(끊기고)
+				*
+				*/
 				while (total >= k) {
 					if (total == k) ans++;
 					total -= q.front();
@@ -41,13 +42,14 @@ int main() {
 				while (!q.empty()) {
 					q.pop();
 				}
-				total = 0;
-			}
-			/*if (total == k) {
-				ans++;
-				q.pop();
+
+				total = 0; 
 			} 
-			else */
+
+			/*
+			*  signal값을 queue에 넣어주면서 k값이 될 수 있는지 확인
+			*  총합이 k보다 크냐, 작냐를 확인해주면 됨
+			*/
 			if (total< k) {
 				total += signal;
 				q.push(signal);
@@ -70,8 +72,7 @@ int main() {
 					total -= q.front();
 					q.pop();
 				}
-			}
-			//cout << "total = " << total << endl;
+			} 
 		}
 		cout << ans << "\n";
 		while (!q.empty()) q.pop();
